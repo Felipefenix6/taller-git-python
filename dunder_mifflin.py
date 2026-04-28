32966bbe791d35d0911c68e3dec0e4ceb7cf0074
Felipefenix6 <161530339+Felipefenix6@users.noreply.github.com>
2026-04-28 14:46:58 -0400

    Create dunder_mifflin.py

---
+++ dunder_mifflin.py
@@ -0,0 +1,8 @@
+import random
+personajes = [“Dwight”, “Jim”, “Creed”]
+acciones = [“puso mi grapadora en gelatina”, “declaró bancarrota”]
+print(“Problema:”, random.choice(personajes), random.choice(acciones))
+# En la terminal:
+git add dunder_mifflin.py
+git commit -m “Crea lógica inicial de excusas”
+git push origin main
